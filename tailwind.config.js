dd0c5fd5f7f42ddb9db3fa1010a0f0008163d660
Sukhpreet singh <101046716+Sukhpreet7137@users.noreply.github.com>
2023-11-13 18:49:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,29 @@ module.exports = {
   content: ["./dist/*.html"],
   darkMode:'class',
   theme: {
+    screens: {
+      'x-sm': '240px',
+      // => @media (min-width: 640px) { ... }
+
+      'sm': '620px',
+      // => @media (min-width: 768px) { ... }
+
+      'md':'650px',
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         ubuntu: ['Ubuntu'],
       }
+      
     },
   },
   plugins: [],
